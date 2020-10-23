5d5c5083d5ba5f97b5a96b17f21d181b59808fb0
Jakub Sewiło <jakub.sewilo@microsoft.wsei.edu.pl>
2020-10-23 10:51:24 +0200

    Finish lab1

---
+++ Lab1Web/Views/Company/CompanyAdded.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab1Web.Views.Company
+{
+    public class CompanyAddedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
